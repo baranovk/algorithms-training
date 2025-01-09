8b383e459da3fe48ed990d628f20bd4012bb341d
kirill.baranov@awara-it.com <kirill.baranov@awara-it.com>
2025-01-09 18:02:29 +0300

    Старт задачи 62. Unique Paths

---
+++ AlgorithmsTraining.Tests/Arrays/UniquePathsTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using AlgorithmsTraining.Arrays;
+
+namespace AlgorithmsTraining.Tests.Arrays;
+
+internal class UniquePathsTests
+{
+    [TestCaseSource(nameof(TestCases))]
+    public int Solution_Should_CountUniquePaths(int m, int n)
+    {
+        return UniquePaths.Solution(m, n);
+    }
+
+    private static IEnumerable TestCases()
+    {
+        yield return new TestCaseData(3, 7).Returns(28);
+        yield return new TestCaseData(3, 2).Returns(3);
+    }
+}
