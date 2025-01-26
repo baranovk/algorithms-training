9f541dcc30b4b4b187f70d5195e7c3b4c7ca85f2
baranovk <baranoffk@gmail.com>
2025-01-26 15:26:22 +0300

    Классы для задачи 18. 4Sum

---
+++ AlgorithmsTraining.Tests/Numbers/FourSumTests.cs
@@ -0,0 +1,33 @@
+﻿using AlgorithmsTraining.Numbers;
+using static AlgorithmsTraining.Tests.Utility;
+
+namespace AlgorithmsTraining.Tests.Numbers;
+
+internal class FourSumTests
+{
+    [Test]
+    public void FourSum_Should_FindFourSum_0()
+    {
+        int[] nums = [1, 0, -1, 0, -2, 2];
+        int target = 0;
+
+        var result = _4Sum.Solution(nums, target).Select(r => r.ToArray()).ToArray();
+
+        Assert.That(result.Count, Is.EqualTo(3));
+        Assert.That(result.Any(r => ArraysAreEqual(r, [-2, -1, 1, 2])), Is.True);
+        Assert.That(result.Any(r => ArraysAreEqual(r, [-2, 0, 0, 2])), Is.True);
+        Assert.That(result.Any(r => ArraysAreEqual(r, [-1, 0, 0, 1])), Is.True);
+    }
+
+    [Test]
+    public void FourSum_Should_FindFourSum_1()
+    {
+        int[] nums = [2, 2, 2, 2, 2];
+        int target = 8;
+
+        var result = _4Sum.Solution(nums, target).Select(r => r.ToArray()).ToArray();
+
+        Assert.That(result.Count, Is.EqualTo(1));
+        Assert.That(result.Any(r => ArraysAreEqual(r, [2, 2, 2, 2, 2])), Is.True);
+    }
+}
