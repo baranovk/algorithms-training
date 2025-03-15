84ef18a0a2984338b5de8bcde79975d58e1f1327
baranovk <baranovk@yandex.ru>
2025-03-15 09:53:56 +0300

    Классы для решения задачи

---
+++ AlgorithmsTraining.Tests/Numbers/CombinationSumTests.cs
@@ -0,0 +1,19 @@
+﻿using static AlgorithmsTraining.Tests.Utility;
+
+namespace AlgorithmsTraining.Tests.Numbers;
+
+internal class CombinationSumTests
+{
+    public void CombinationSum_Should_FindCombinations()
+    {
+        //int[] nums = [1, 0, -1, 0, -2, 2];
+        //int target = 0;
+
+        //var result = _4Sum.Solution(nums, target).Select(r => r.ToArray()).ToArray();
+
+        //Assert.That(result.Count, Is.EqualTo(3));
+        //Assert.That(result.Any(r => ArraysAreEqualWithoutOrder(r, [-2, -1, 1, 2])), Is.True);
+        //Assert.That(result.Any(r => ArraysAreEqualWithoutOrder(r, [-2, 0, 0, 2])), Is.True);
+        //Assert.That(result.Any(r => ArraysAreEqualWithoutOrder(r, [-1, 0, 0, 1])), Is.True);
+    }
+}
