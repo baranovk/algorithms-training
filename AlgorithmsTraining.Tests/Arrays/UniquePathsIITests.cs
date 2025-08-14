f0112cb6d4fe0d539f3e8672e90c3fb83aac03be
baranovk <baranovk@yandex.ru>
2025-08-14 13:17:34 +0300

    Решение задачи "63. Unique Paths II"

---
+++ AlgorithmsTraining.Tests/Arrays/UniquePathsIITests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using AlgorithmsTraining.Arrays;
+
+namespace AlgorithmsTraining.Tests.Arrays;
+
+internal class UniquePathsIITests
+{
+    [TestCaseSource(nameof(TestCases))]
+    public int Solution_Should_CountUniquePaths(int[][] obstacleGrid)
+    {
+        return UniquePathsII.UniquePathsWithObstacles(obstacleGrid);
+    }
+
+    private static IEnumerable TestCases()
+    {
+        yield return new TestCaseData([new int[][] { [0, 0, 0, 1], [0, 0, 1, 0], [0, 0, 0, 0] }]).Returns(3);
+        yield return new TestCaseData([new int[][] { [0, 0, 0], [0, 1, 0], [0, 0, 0] }]).Returns(2);
+        yield return new TestCaseData([new int[][] { [0, 1], [0, 0] }]).Returns(1);
+        yield return new TestCaseData([new int[][] { [0] }]).Returns(1);
+    }
+}
