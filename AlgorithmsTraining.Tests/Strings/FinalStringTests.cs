98acb2a16b1b2b4bfc3f1bb4c9c44d35d9b225df
baranovk <baranovk@yandex.ru>
2025-08-21 13:26:45 +0300

    Решение задачи "2810. Faulty Keyboard"

---
+++ AlgorithmsTraining.Tests/Strings/FinalStringTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using AlgorithmsTraining.Strings;
+
+namespace AlgorithmsTraining.Tests.Strings;
+
+internal class FinalStringTests
+{
+    [TestCaseSource(nameof(TestCases))]
+    public string FaultyKeyboard_Tests(string s) => FaultyKeyboard.FinalString(s);
+
+    private static IEnumerable TestCases()
+    {
+        yield return new TestCaseData("string").Returns("rtsng");
+        yield return new TestCaseData("poiinter").Returns("ponter");
+        yield return new TestCaseData("viwif").Returns("wvf");
+    }
+}
---
+++ AlgorithmsTraining/Strings/FaultyKeyboard.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+
+namespace AlgorithmsTraining.Strings;
+
+public static class FaultyKeyboard
+{
+    /*
+     * 2810. Faulty Keyboard
+
+    Your laptop keyboard is faulty, and whenever you type a character 'i' on it, it reverses the string that you have written. 
+    Typing other characters works as expected.
+
+    You are given a 0-indexed string s, and you type each character of s using your faulty keyboard.
+    Return the final string that will be present on your laptop screen.
+
+    Example 1:
+    
+    Input: s = "string"
+    Output: "rtsng"
+    Explanation: 
+    After typing first character, the text on the screen is "s".
+    After the second character, the text is "st". 
+    After the third character, the text is "str".
+    Since the fourth character is an 'i', the text gets reversed and becomes "rts".
+    After the fifth character, the text is "rtsn". 
+    After the sixth character, the text is "rtsng". 
+    Therefore, we return "rtsng".
+
+    Example 2:
+    
+    Input: s = "poiinter"
+    Output: "ponter"
+    Explanation: 
+    After the first character, the text on the screen is "p".
+    After the second character, the text is "po". 
+    Since the third character you type is an 'i', the text gets reversed and becomes "op". 
+    Since the fourth character you type is an 'i', the text gets reversed and becomes "po".
+    After the fifth character, the text is "pon".
+    After the sixth character, the text is "pont". 
+    After the seventh character, the text is "ponte". 
+    After the eighth character, the text is "ponter". 
+    Therefore, we return "ponter".
+    
+    Constraints:
+
+    [1] 1 <= s.length <= 100
+    [2] s consists of lowercase English letters.
+    [3] s[0] != 'i'
+
+    Runtime
+    6 ms
+    Beats 53.06%
+
+    Memory
+    52.38 MB
+    Beats 59.18%
+     */
+
+    public static string FinalString(string s)
+    {
+        if (1 == s.Length) { return s; }
+
+        var sChars = s.ToCharArray();
+
+        for (int i = 1; i < sChars.Length; i++)
+        {
+            if ('i' == sChars[i]) { ReverseString(sChars.AsSpan(0, i + 1)); }
+        }
+
+        var sb = new StringBuilder();
+
+        for (var i = 0; i < sChars.Length; i++) 
+        {
+            if ('i' != sChars[i]) { sb.Append(sChars[i]); }
+        }
+
+        return sb.ToString();
+    }
+
+    private static void ReverseString(Span<char> s)
+    {
+        if (1 == s.Length) { return; }
+
+        for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+        {
+            (s[j], s[i]) = (s[i], s[j]);
+        }
+    }
+}
