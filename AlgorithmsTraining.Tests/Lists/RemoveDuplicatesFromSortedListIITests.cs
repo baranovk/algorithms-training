3ec3cdcd8fff99dd811d12d86a03a56d0e5bdcfc
baranovk <baranovk@yandex.ru>
2025-09-06 21:17:19 +0300

    Решение задачи "82. Remove Duplicates from Sorted List II"

---
+++ AlgorithmsTraining.Tests/Lists/RemoveDuplicatesFromSortedListIITests.cs
@@ -0,0 +1,57 @@
+﻿using AlgorithmsTraining.Lists;
+
+namespace AlgorithmsTraining.Tests.Lists;
+
+internal class RemoveDuplicatesFromSortedListIITests
+{
+    [Test]
+    public void RemoveDuplicatesFromSortedListII_Test_1()
+    {
+        var head = new ListNode(1, new ListNode(2, new ListNode(3)));
+        head = RemoveDuplicatesFromSortedListII.DeleteDuplicates(head);
+
+        Assert.That(ListUtils.GetNodeValue(head, 0), Is.EqualTo(1));
+        Assert.That(ListUtils.GetNodeValue(head, 1), Is.EqualTo(2));
+        Assert.That(ListUtils.GetNodeValue(head, 2), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void RemoveDuplicatesFromSortedListII_Test_2()
+    {
+        var head = new ListNode(1, new ListNode(1, new ListNode(1)));
+        head = RemoveDuplicatesFromSortedListII.DeleteDuplicates(head);
+
+        Assert.That(head, Is.Null);
+    }
+
+    [Test]
+    public void RemoveDuplicatesFromSortedListII_Test_3()
+    {
+        var head = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(3))));
+        head = RemoveDuplicatesFromSortedListII.DeleteDuplicates(head);
+
+        Assert.That(ListUtils.GetNodeValue(head, 0), Is.EqualTo(1));
+        Assert.That(ListUtils.GetNodeValue(head, 1), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void RemoveDuplicatesFromSortedListII_Test_4()
+    {
+        var head = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3))));
+        head = RemoveDuplicatesFromSortedListII.DeleteDuplicates(head);
+
+        Assert.That(ListUtils.GetNodeValue(head, 0), Is.EqualTo(2));
+        Assert.That(ListUtils.GetNodeValue(head, 1), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void RemoveDuplicatesFromSortedListII_Test_5()
+    {
+        var head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3, new ListNode(4, new ListNode(4, new ListNode(5)))))));
+        head = RemoveDuplicatesFromSortedListII.DeleteDuplicates(head);
+
+        Assert.That(ListUtils.GetNodeValue(head, 0), Is.EqualTo(1));
+        Assert.That(ListUtils.GetNodeValue(head, 1), Is.EqualTo(2));
+        Assert.That(ListUtils.GetNodeValue(head, 2), Is.EqualTo(5));
+    }
+}
---
+++ AlgorithmsTraining/Lists/RemoveDuplicatesFromSortedListII.cs
@@ -0,0 +1,56 @@
+﻿namespace AlgorithmsTraining.Lists;
+
+/*
+ * 82. Remove Duplicates from Sorted List II
+
+   Given the head of a sorted linked list, delete all nodes that have duplicate numbers, 
+   leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+   Example 1:
+
+   Input: head = [1,2,3,3,4,4,5]
+   Output: [1,2,5]
+
+   Example 2:
+
+   Input: head = [1,1,1,2,3]
+   Output: [2,3]
+
+   Constraints:
+
+    [1] The number of nodes in the list is in the range [0, 300].
+    [2] -100 <= Node.val <= 100
+    [3] The list is guaranteed to be sorted in ascending order.
+
+    Runtime
+    0 ms
+    Beats 100.00%
+
+    Memory
+    43.41 MB
+    Beats 41.95%
+ */
+public static class RemoveDuplicatesFromSortedListII
+{
+    public static ListNode DeleteDuplicates(ListNode head)
+    {
+        if (null == head || null == head.next) { return head; }
+        ListNode prevnode = new(-1, head), inode = head, jnode = head;
+
+        do
+        {
+            if (jnode.val == jnode.next?.val) { jnode = jnode.next; continue; }
+
+            // ASSERT: (null == jnode.next || jnode.val != jnode.next.val)
+            if (inode != jnode)
+            {
+                head = head == inode ? jnode.next : head;
+                prevnode.next = inode = jnode = jnode.next;
+            }
+            else { prevnode = inode; prevnode.next = inode = jnode = jnode.next;  }
+        }
+        while (null != jnode);
+
+        return head;
+    }
+}
