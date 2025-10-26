f75983e3c594049749f99f5b1b990d4b7313ddf0
baranovk <baranoffk@gmail.com>
2025-10-26 19:15:10 +0300

    Решение "98. Validate Binary Search Tree"

---
+++ AlgorithmsTraining.Tests/Trees/ValidateBinarySearchTreeTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using AlgorithmsTraining.Trees;
+
+namespace AlgorithmsTraining.Tests.Trees;
+
+internal class ValidateBinarySearchTreeTests
+{
+    [TestCaseSource(nameof(TestCases))]
+    public bool ValidateBinarySearchTree_Tests(object[] values)
+    {
+        return ValidateBinarySearchTree.IsValidBST(TreeUtils.BuildBinaryTree(values));
+    }
+
+    private static IEnumerable TestCases()
+    {
+        yield return new TestCaseData([new object[] { 2, 2, 2 }]).Returns(false);
+        yield return new TestCaseData([new object[] { 2, 1, 3 }]).Returns(true);
+        yield return new TestCaseData([new object[] { 5, 1, 4, null, null, 3, 6 }]).Returns(false);
+        yield return new TestCaseData([new object[] { 5, 4, 6, null, null, 3, 7 }]).Returns(false);
+        yield return new TestCaseData([new object[] { 45, 42, null, null, 44, 43, null, 41, null }]).Returns(false);
+        yield return new TestCaseData([new object[] { 3, 1, 5, 0, 2, 4, 6, null, null, null, 3 }]).Returns(false);
+    }
+}
