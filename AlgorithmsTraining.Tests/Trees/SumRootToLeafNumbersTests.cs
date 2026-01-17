f41af288b39b928f2b1f55a5e51c8dce9c1b584c
baranovk <baranoffk@gmail.com>
2026-01-17 15:06:04 +0300

    Решение задачи "129. Sum Root to Leaf Numbers"

---
+++ AlgorithmsTraining.Tests/Trees/SumRootToLeafNumbersTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using AlgorithmsTraining.Trees;
+
+namespace AlgorithmsTraining.Tests.Trees
+{
+    internal class SumRootToLeafNumbersTests
+    {
+        [TestCaseSource(nameof(TestCases))]
+        public int SumRootToLeafNumbers_Tests(object[] treeDescription)
+        {
+            return SumRootToLeafNumbers.SumNumbers(TreeUtils.BuildBinaryTree(treeDescription));
+        }
+
+        private static IEnumerable TestCases()
+        {
+            yield return new TestCaseData([new object[] { 1 }]).Returns(1);
+            yield return new TestCaseData([new object[] { 1, 2, 3 }]).Returns(25);
+            yield return new TestCaseData([new object[] { 1, 2, 3, 4, null }]).Returns(137);
+            yield return new TestCaseData([new object[] { 1, 2, 3, 4, 5, null, null, 6, 7 }]).Returns(2631);
+            yield return new TestCaseData([new object[] { 4, 9, 0, 5, 1 }]).Returns(1026);
+        }
+    }
+}
