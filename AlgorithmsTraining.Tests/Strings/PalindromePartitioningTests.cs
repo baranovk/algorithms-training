a498d1977c382f5c70bf6b313a98bbdac65c6563
baranovk <baranoffk@gmail.com>
2026-02-10 18:53:59 +0300

    Решение задачи "131. Palindrome Partitioning"

---
+++ AlgorithmsTraining.Tests/Strings/PalindromePartitioningTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using AlgorithmsTraining.Strings;
+
+namespace AlgorithmsTraining.Tests.Strings
+{
+    internal class PalindromePartitioningTests
+    {
+        [TestCaseSource(nameof(TestCases))]
+        public void PalindromePartitioning_Tests(string s, IList<IList<string>> expectedResult)
+        {
+            var result = PalindromePartitioning.Partition(s);
+            Assert.That(result, Is.EquivalentTo(expectedResult));
+        }
+
+        private static IEnumerable TestCases()
+        {
+            yield return new TestCaseData(
+                "ccaacabacb",
+                new List<IList<string>> {
+                    new List<string> { "c","c","a","a","c","a","b","a","c","b"}, new List<string> { "c","c","a","a","c","aba","c","b"}, new List<string> { "c","c","a","a","cabac","b"}, new List<string> { "c","c","a","aca","b","a","c","b"}, new List<string> { "c","c","aa","c","a","b","a","c","b"}, new List<string> { "c","c","aa","c","aba","c","b"}, new List<string> { "c","c","aa","cabac","b"}, new List<string> { "c","caac","a","b","a","c","b"}, new List<string> { "c","caac","aba","c","b"}, new List<string> { "cc","a","a","c","a","b","a","c","b"}, new List<string> { "cc","a","a","c","aba","c","b"}, new List<string> { "cc","a","a","cabac","b"}, new List<string> { "cc","a","aca","b","a","c","b"}, new List<string> { "cc","aa","c","a","b","a","c","b"}, new List<string> { "cc","aa","c","aba","c","b"}, new List<string> { "cc","aa","cabac","b" }
+                }
+            );
+
+            yield return new TestCaseData(
+                "aa",
+                new List<IList<string>> {
+                    new List<string> { "a", "a" }, new List<string> { "aa" }
+                }
+            );
+
+            yield return new TestCaseData(
+                "aaa",
+                new List<IList<string>> {
+                    new List<string> { "a", "a", "a" }, new List<string> { "a", "aa" },
+                    new List<string> { "aa", "a" }, new List<string> { "aaa" }
+                }
+            );
+
+            yield return new TestCaseData(
+                "aba",
+                new List<IList<string>> {
+                    new List<string> { "a", "b", "a" }, new List<string> { "aba" }
+                }
+            );
+
+            yield return new TestCaseData(
+                "abba",
+                new List<IList<string>> {
+                    new List<string> { "a", "b", "b", "a" }, new List<string> { "abba" },
+                    new List<string> { "a", "bb", "a" },
+                }
+            );
+
+            yield return new TestCaseData(
+                "aab",
+                new List<IList<string>> {
+                    new List<string> { "a","a","b" }, new List<string> { "aa","b" }
+                }
+            );
+
+            yield return new TestCaseData(
+                "a",
+                new List<IList<string>> {
+                    new List<string> { "a" }
+                }
+            );
+        }
+    }
+}
---
+++ AlgorithmsTraining/Strings/PalindromePartitioning.cs
@@ -0,0 +1,109 @@
+﻿namespace AlgorithmsTraining.Strings
+{
+    /*
+     * 131. Palindrome Partitioning
+
+       Given a string s, partition s such that every of the partition is a . Return all possible palindrome partitioning of s.
+       
+       Example 1:
+       
+       Input: s = "aab"
+       Output: [["a","a","b"],["aa","b"]]
+       
+       Example 2:
+       
+       Input: s = "a"
+       Output: [["a"]]
+       
+       Constraints:
+       
+         [1] 1 <= s.length <= 16
+         [2] s contains only lowercase English letters.
+
+        Runtime
+        27 ms
+        Beats 23.67%
+
+        Memory
+        84.69 MB
+        Beats 9.19%
+     */
+    public static class PalindromePartitioning  // "ccaacabacb"
+    {
+        private const char PalindromeMark = 'P';
+
+        public static IList<IList<string>> Partition(string s)
+        {
+            char[,] matrix = new char[s.Length, s.Length];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = i; j < s.Length; j++)
+                {
+                    if(IsPalindrome(matrix, i, j, s))
+                    {
+                        matrix[i, j] = PalindromeMark;
+                        TryExpandPalindrome(s, matrix, i, j);
+                    }
+                }
+            }
+
+            var result = new List<IList<string>>();
+            PrintMatrix(matrix);
+            CollectPartitions(s, matrix, 0, Enumerable.Empty<string>().ToList(), result);
+            return result;
+        }
+
+        private static void TryExpandPalindrome(string s, char[,] matrix, int i, int j)
+        {
+            if (i > 0 && j < s.Length - 1 && s[i - 1] == s[j + 1])
+            {
+                matrix[i - 1, j + 1] = PalindromeMark;
+                TryExpandPalindrome(s, matrix, i - 1, j + 1);
+            }
+        }
+
+        private static bool IsPalindrome(char[,] matrix, int i, int j, string s)
+        {
+            return i == j || (PalindromeMark == matrix[i, j - 1] && s[i] == s[j]);
+        }
+
+        private static void CollectPartitions(string s, char[,] matrix, int position, 
+            List<string> prevPartitions, List<IList<string>> accumulator)
+        {
+            for (int i = position; i < s.Length; i++)
+            {
+                if (PalindromeMark == matrix[position, i])
+                {
+                    var nextPartitions = new List<string>();
+                    nextPartitions.AddRange(prevPartitions);
+                    nextPartitions.Add(s.Substring(position, i - position + 1));
+
+                    if (i < s.Length - 1)
+                    {
+                        CollectPartitions(s, matrix, i + 1, nextPartitions, accumulator);
+                    }
+                    else
+                    {
+                        accumulator.Add(nextPartitions);
+                    }
+                }
+            }
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(default == matrix[i, j] ? 'O' : matrix[i, j]);
+                    Console.Write('\t');
+                }
+
+                Console.Write('\r');
+                Console.Write('\n');
+            }
+        }
+    }
+}
